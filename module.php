8f938df1846fd02e02c74076cfe38fbad5f50407
nadine-shi <nadine@afterlogic.com>
2016-08-25 10:29:27 +0300

    System - create checkUserRoleIsAtLeast method in Api

--- module.php
+++ module.php
@@ -4,6 +4,8 @@ class OpenPgpWebclientModule extends AApiModule
 {
 	public function GetAppData()
 	{
+		\CApi::checkUserRoleIsAtLeast(\EUserRole::Anonymous);
+		
 		return array(
 			'EnableModule' => true // AppData.User.EnableOpenPgp
 		);
