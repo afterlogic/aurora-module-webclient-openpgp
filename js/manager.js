ab47cc7801f6970c0e34aea05143647620e64b72
nadine-shi <nadine@afterlogic.com>
2016-09-13 17:27:28 +0300

    System - use manager.js files instead of manager-smth.js

--- js/manager.js
+++ js/manager.js
@@ -6,28 +6,32 @@ function IsPgpSupported()
 }
 
 module.exports = function (oAppData) {
-	var
-		_ = require('underscore'),
-		
-		TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
-		
-		Settings = require('modules/%ModuleName%/js/Settings.js'),
-		oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {})
-	;
+	var App = require('%PathToCoreWebclientModule%/js/App.js');
 	
-	Settings.init(oSettings);
-	
-	return {
-		isAvailable: function (iUserRole, bPublic) {
-			return !bPublic && iUserRole === Enums.UserRole.NormalUser;
-		},
-		start: function (ModulesManager) {
-			if (IsPgpSupported())
-			{
-				ModulesManager.run('MailWebclient', 'registerMessagePaneController', [require('modules/%ModuleName%/js/views/MessageControlsView.js'), 'BeforeMessageHeaders']);
-				ModulesManager.run('MailWebclient', 'registerComposeToolbarController', [require('modules/%ModuleName%/js/views/ComposeButtonsView.js')]);
-				ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/OpenPgpSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
+	if (App.getUserRole() === Enums.UserRole.NormalUser)
+	{
+		var
+			_ = require('underscore'),
+
+			TextUtils = require('%PathToCoreWebclientModule%/js/utils/Text.js'),
+
+			Settings = require('modules/%ModuleName%/js/Settings.js'),
+			oSettings = _.extend({}, oAppData[Settings.ServerModuleName] || {}, oAppData['%ModuleName%'] || {})
+		;
+
+		Settings.init(oSettings);
+
+		return {
+			start: function (ModulesManager) {
+				if (IsPgpSupported())
+				{
+					ModulesManager.run('MailWebclient', 'registerMessagePaneController', [require('modules/%ModuleName%/js/views/MessageControlsView.js'), 'BeforeMessageHeaders']);
+					ModulesManager.run('MailWebclient', 'registerComposeToolbarController', [require('modules/%ModuleName%/js/views/ComposeButtonsView.js')]);
+					ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/OpenPgpSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
+				}
 			}
-		}
-	};
+		};
+	}
+	
+	return null;
 };
