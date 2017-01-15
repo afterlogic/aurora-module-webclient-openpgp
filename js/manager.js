67a1e56f209fc38b6dcffacfe7a04ba3541ca766
nadine-shi <nadine@afterlogic.com>
2017-01-15 21:29:11 +0300

    Files - ability to add action from another module

--- js/manager.js
+++ js/manager.js
@@ -6,7 +6,14 @@ function IsPgpSupported()
 }
 
 module.exports = function (oAppData) {
-	var App = require('%PathToCoreWebclientModule%/js/App.js');
+	var
+		Utils = require('%PathToCoreWebclientModule%/js/utils/Common.js'),
+		Ajax = require('%PathToCoreWebclientModule%/js/Ajax.js'),
+		App = require('%PathToCoreWebclientModule%/js/App.js'),
+		
+		Popups = require('%PathToCoreWebclientModule%/js/Popups.js'),
+		ImportKeyPopup = require('modules/%ModuleName%/js/popups/ImportKeyPopup.js')
+	;
 	
 	if (App.getUserRole() === Enums.UserRole.NormalUser)
 	{
@@ -28,6 +35,24 @@ module.exports = function (oAppData) {
 					ModulesManager.run('MailWebclient', 'registerMessagePaneController', [require('modules/%ModuleName%/js/views/MessageControlsView.js'), 'BeforeMessageHeaders']);
 					ModulesManager.run('MailWebclient', 'registerComposeToolbarController', [require('modules/%ModuleName%/js/views/ComposeButtonsView.js')]);
 					ModulesManager.run('SettingsWebclient', 'registerSettingsTab', [function () { return require('modules/%ModuleName%/js/views/OpenPgpSettingsPaneView.js'); }, Settings.HashModuleName, TextUtils.i18n('%MODULENAME%/LABEL_SETTINGS_TAB')]);
+					
+					App.subscribeEvent('FilesWebclient::ParseFile::after', function (oFile) {
+						if (oFile && _.isFunction(oFile.addAction) && Utils.getFileExtension(oFile.fileName()) === 'asc')
+						{
+							var oActionData = {
+								'Text': TextUtils.i18n('%MODULENAME%/ACTION_FILE_IMPORT_KEY'),
+								'Handler': function () {
+									Ajax.send('Files', 'DownloadFile', {'Type':oFile.storageType(),'Name':oFile.fileName(),'Path':oFile.path()}, function (oResponse) {
+										if (oResponse.ResponseText)
+										{
+											Popups.showPopup(ImportKeyPopup, [oResponse.ResponseText]);
+										}
+									}, this, undefined, {'Format': 'Raw'});
+								}
+							};
+							oFile.addAction('import', true, oActionData);
+						}
+					});
 				}
 			}
 		};
