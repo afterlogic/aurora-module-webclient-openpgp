0ead482dbf82d5030638c635dd8853bb97dc3135
sash04ek <a.ovcharov@gmail.com>
2017-03-07 11:23:36 +0300

    added

--- Module.php
+++ Module.php
@@ -2,7 +2,7 @@
 
 namespace Aurora\Modules\OpenPgpWebclient;
 
-class Module extends \Aurora\System\Module\AbstractModule
+class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
 	/***** public functions might be called with web API *****/
 	/**
