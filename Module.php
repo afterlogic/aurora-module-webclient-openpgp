45c5af57b0fa82eaacec820b67f94eafd05ea5a1
sash04ek <a.ovcharov@gmail.com>
2020-04-01 13:23:19 +0300

    Added PGP public keys management

--- Module.php
+++ Module.php
@@ -21,13 +21,26 @@ class Module extends \Aurora\System\Module\AbstractWebclientModule
 		\Aurora\Modules\Core\Classes\User::extend(
 			self::GetName(),
 			[
-				'EnableModule'	=> array('bool', false),
+				'EnableModule'	=> ['bool', false],
 			]
-		);		
+		);
+
+		\Aurora\Modules\Contacts\Classes\Contact::extend(
+			self::GetName(),
+			[
+				'PgpKey'	=> ['text', null],
+			]
+
+		);
 		
 		$this->subscribeEvent('Files::PopulateFileItem::after', array($this, 'onAfterPopulateFileItem'));
 		$this->subscribeEvent('Mail::GetBodyStructureParts', array($this, 'onGetBodyStructureParts'));
 		$this->subscribeEvent('Mail::ExtendMessageData', array($this, 'onExtendMessageData'));
+		$this->subscribeEvent('Contacts::CreateContact::after', array($this, 'onAfterCreateOrUpdateContact'));
+		$this->subscribeEvent('Contacts::UpdateContact::after', array($this, 'onAfterCreateOrUpdateContact'));		
+		$this->subscribeEvent('Contacts::GetContacts::after', array($this, 'onAfterGetContacts'));		
+
+		$this->subscribeEvent('Contacts::Contact::ToResponseArray', array($this, 'onContactsContactToResponseArray'));		
 	}
 	
 	/**
@@ -84,6 +97,48 @@ class Module extends \Aurora\System\Module\AbstractWebclientModule
 			}
 		}
 	}
+
+	public function onAfterCreateOrUpdateContact($aArgs, &$mResult)
+	{
+		if (isset($mResult['UUID']) && isset($aArgs['Contact']['PublicPgpKey']))
+		{
+			$sPublicPgpKey = $aArgs['Contact']['PublicPgpKey'];
+			if (empty(\trim($sPublicPgpKey)))
+			{
+				$sPublicPgpKey = null;
+			}
+			$oContact = \Aurora\Modules\Contacts\Module::Decorator()->GetContact($mResult['UUID'], $aArgs['UserId']);
+			if ($oContact instanceof \Aurora\Modules\Contacts\Classes\Contact && $oContact->{$this->GetName() . '::PgpKey'} !== $sPublicPgpKey)
+			{
+				$oContact->{$this->GetName() . '::PgpKey'} = $sPublicPgpKey;
+				\Aurora\Modules\Contacts\Module::Decorator()->UpdateContactObject($oContact);
+			}			
+		}		
+	}	
+
+	public function onContactsContactToResponseArray($aArgs, &$mResult)
+	{
+		if (isset($mResult[$this->GetName() . '::PgpKey']))
+		{
+			$mResult['PublicPgpKey'] = $mResult[$this->GetName() . '::PgpKey'];
+			unset($mResult[$this->GetName() . '::PgpKey']);
+		}
+	}
+	
+	public function onAfterGetContacts($aArgs, &$mResult)
+	{
+		if (isset($mResult['List']))
+		{
+			$aContactUUIDs = array_map(function ($aValue) {
+				return $aValue['UUID'];
+			}, $mResult['List']);
+			$aContactsInfo = $this->GetContactsWithPublicKeys($aArgs['UserId'], $aContactUUIDs);
+			foreach ($mResult['List'] as &$aContact)
+			{
+				$aContact['HasPgpPublicKey'] = $aContactsInfo[$aContact['UUID']];
+			}
+		}	
+	}
 	
 	/***** public functions might be called with web API *****/
 	/**
@@ -127,6 +182,155 @@ class Module extends \Aurora\System\Module\AbstractWebclientModule
 		}
 		
 		return false;
+	}
+
+	public function AddPublicKeyToContact($UserId, $Email, $Key)
+	{
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+
+		$aContacts = \Aurora\Modules\Contacts\Module::Decorator()->GetContactsByEmails(
+			$UserId, 
+			\Aurora\Modules\Contacts\Enums\StorageType::Personal, 
+			[$Email]
+		);
+
+		if (count($aContacts) === 0)
+		{
+			$mResult = \Aurora\Modules\Contacts\Module::Decorator()->CreateContact(
+				['PersonalEmail' => $Email],
+				$UserId
+			);	
+			if (isset($mResult['UUID']))
+			{
+				$oContact = \Aurora\Modules\Contacts\Module::Decorator()->GetContact($mResult['UUID'], $UserId);
+				if ($oContact instanceof \Aurora\Modules\Contacts\Classes\Contact)
+				{
+					$aContacts = [$oContact];
+				}
+			}
+		}
+
+		if (is_array($aContacts) && count($aContacts) > 0)
+		{
+			foreach ($aContacts as $oContact)
+			{
+				if ($oContact instanceof \Aurora\Modules\Contacts\Classes\Contact)
+				{
+					$oContact->{$this->GetName() . '::PgpKey'} = $Key;
+					\Aurora\Modules\Contacts\Module::Decorator()->UpdateContactObject($oContact);
+				}
+			}
+
+			return true;
+		}
+	}
+
+	public function RemovePublicKeyFromContact($UserId, $Email)
+	{
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+
+		$aContacts = \Aurora\Modules\Contacts\Module::Decorator()->GetContactsByEmails(
+			$UserId, 
+			\Aurora\Modules\Contacts\Enums\StorageType::Personal, 
+			[$Email]
+		);
+
+		if (is_array($aContacts) && count($aContacts) > 0)
+		{
+			foreach ($aContacts as $oContact)
+			{
+				if ($oContact instanceof \Aurora\Modules\Contacts\Classes\Contact)
+				{
+					$oContact->{$this->GetName() . '::PgpKey'} = null;
+					\Aurora\Modules\Contacts\Module::Decorator()->UpdateContactObject($oContact);
+				}
+			}
+
+			return true;
+		}		
+	}
+
+	public function GetPublicKeysByCountactUUIDs($UserId, $ContactUUIDs)
+	{
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+
+		$aResult = [];
+
+		if (count($ContactUUIDs))
+		{
+			$aContacts = \Aurora\Modules\Contacts\Module::Decorator()->GetContactsByUids($UserId, $ContactUUIDs);
+			if (is_array($aContacts) && count($aContacts) > 0)
+			{
+				foreach ($aContacts as $oContact)
+				{
+					$aResult[] = [
+						'Email' => $oContact->ViewEmail,
+						'PublicPgpKey' => $oContact->{$this->GetName() . '::PgpKey'}
+					];
+				}
+			}	
+		}
+
+		return $aResult;
+	}
+
+	public function GetContactsWithPublicKeys($UserId, $UUIDs)
+	{
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+		$aResult = [];
+
+		$aContactsInfo = \Aurora\Modules\Contacts\Module::Decorator()->GetContactsInfo(
+			\Aurora\Modules\Contacts\Enums\StorageType::Personal,
+			$UserId,
+			[
+				'$AND' => [
+					$this->GetName() . '::PgpKey' => ['NULL', 'IS NOT'],
+					'UUID' => [$UUIDs, 'IN']
+				]
+			]
+		);	
+		$aContactUUIDs = [];
+		if (isset($aContactsInfo['Info']) && count($aContactsInfo['Info']) > 0)
+		{
+			$aContactUUIDs = array_map(function ($aValue) {
+				return $aValue['UUID'];
+			}, $aContactsInfo['Info']);
+		}
+		foreach ($UUIDs as $sUUID)
+		{
+			$mResult[$sUUID] = in_array($sUUID, $aContactUUIDs) ? true : false;
+		}
+
+		return $mResult;
+	}
+
+	public function GetPublicKeysFromContacts($UserId)
+	{
+		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
+
+		$aResult = [];
+
+		$aContactsInfo = \Aurora\Modules\Contacts\Module::Decorator()->GetContactsInfo(
+			\Aurora\Modules\Contacts\Enums\StorageType::Personal,
+			$UserId,
+			[
+				'$AND' => [
+					$this->GetName() . '::PgpKey' => ['NULL', 'IS NOT']
+				]
+			]
+		);
+
+		$aContactUUIDs = [];
+		if (is_array($aContactsInfo['Info']) && count($aContactsInfo['Info']) > 0)
+		{
+			$aContactUUIDs = array_map(function ($aValue) {
+				return $aValue['UUID'];
+			}, $aContactsInfo['Info']);
+		}
+		$aResult = $this->Decorator()->GetPublicKeysByCountactUUIDs($UserId, $aContactUUIDs);
+
+
+		return $aResult;
 	}
 	/***** public functions might be called with web API *****/
 }
